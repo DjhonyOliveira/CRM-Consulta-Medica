166bf91a6e53162e1c4a930889d4f5b3de451dbb
DjhonyOliveira <djonatandeoliveira@hotmail.com>
2025-08-21 20:31:38 -0300

    aplicado_componente_modal_delete_rotinas

--- app/Helpers/helpers.php
+++ app/Helpers/helpers.php
@@ -1,5 +1,7 @@
 <?php
 
+use Carbon\Carbon;
+
 /**
  * Realiza o tratamento de um valor string para float
  * @param string $sValor
@@ -20,4 +22,24 @@ function trataValorDecimal(string $sValor): float
 function getUsuarioLogado()
 {
     return auth()->user();
+}
+
+/**
+ * Formata uma string de data para o padrão BR (d/m/a)
+ * @param string $data
+ * @return string
+ */
+function formataDataBr(string $data)
+{
+    return Carbon::parse($data)->format('d/m/Y');
+}
+
+/**
+ * Formata uma string de horário para o padrão BR (h/m)
+ * @param string $horario
+ * @return string
+ */
+function formataHorarioBr(string $horario)
+{
+    return Carbon::parse($horario)->format('H:i');
 }
--- resources/views/livewire/modal-consulta.blade.php
+++ resources/views/livewire/modal-consulta.blade.php
@@ -9,47 +9,13 @@
     @if($showModal)
         @if($action == EnumAcao::delete->value)
             <div>
-                <div id="draggableModal" 
-                    class="fixed top-1/2 left-1/2 w-96 bg-white text-black border border-gray-300 rounded-xl shadow-xl z-50"
-                    style="transform: translate(-50%, -50%);">
-
-                    <div id="modalHeader" class="cursor-move flex justify-between items-center px-4 py-3 border-b bg-gray-100 rounded-t-xl">
-                        <h2 class="text-base font-semibold">Deletar Consulta</h2>
-                        <button
-                            wire:click="closeModal"
-                            class="bg-red-600 text-gray-900 text-lg w-8 h-8 rounded">
-                            &times;
-                        </button>
-                    </div>
-
-                    <form id="formModal" action="{{ route('consulta.view') }}" method="POST">
-                        @csrf
-                        @method('delete')
-
-                        <div class="px-4 py-4">
-                            <p class="text-red-600 text-sm mt-1 error-message" data-field="name"></p>
-                            @if(array_key_exists('id', $consulta))
-                                <input
-                                    type="number"
-                                    name="id"
-                                    value="{{ $consulta['id'] ?? '' }}"
-                                    placeholder="number"
-                                    class="w-full border border-gray-300 rounded-md px-3 py-2 text-sm mb-2 focus:outline-none focus:ring-2 focus:ring-blue-400"
-                                    hidden>
-                            @endif
-
-                            <div class="px-4 pb-4">
-                                <p class="text-sm text-gray-700 mb-3">
-                                    Tem certeza que deseja <span class="font-semibold text-red-600">deletar esta consulta</span>? Esta ação não poderá ser desfeita.
-                                </p>
-                                <button type="submit"
-                                    class="w-full bg-red-600 text-white py-2 px-4 rounded-md hover:bg-red-700 transition">
-                                    Confirmar Exclusão
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
+                @livewire('components.modal-delete', [
+                        "showModalDelete" => $showModal,
+                        "action"          => $action,
+                        "dados"           => $consulta,
+                        "nomeRotina"      => "Consulta",
+                        "route"           => "consulta.view"
+                    ])
             </div>
         @else 
             <div id="draggableModal"
